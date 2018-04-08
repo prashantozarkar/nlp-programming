e9627c1d6f2ec363cecb76575ad66e39367fe29c
Bhushan Gadekar <bhushan@shoppinpal.com>
2018-04-08 20:25:15 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,50 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselModule } from 'ngx-bootstrap';
+import { SliderComponent } from './slider/slider.component';
+import { SectionComponent } from './section/section.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { CounsellingComponent } from './counselling/counselling.component';
 
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'counselling', component: CounsellingComponent },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    SectionComponent,
+    ParagraphComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    CounsellingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
